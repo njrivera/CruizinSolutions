01db4ca5498ad7168f66bff1f4ea6cbdd220aaca
nato <natanael.j.rivera@gmail.com>
2017-08-03 00:46:33 -0400

    rename server file

---
+++ server/server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/CruizinSolutions/server/db"
+	"github.com/CruizinSolutions/server/utilities"
+
+	"github.com/go-martini/martini"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	m := martini.Classic()
+	static := martini.Static("../web")
+	m.Use(static)
+	m.Get("/names", getNamesHandler)
+	m.Post("/create", createCustomer)
+	m.Run()
+}
+
+func getNamesHandler(r *http.Request, w http.ResponseWriter) int {
+	customers := db.GetAllCustumers()
+	js, _ := json.MarshalIndent(customers, "", "  ")
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+	return http.StatusOK
+}
+
+func createCustomer(r *http.Request) int {
+	customer := db.Customer{}
+	err := json.NewDecoder(r.Body).Decode(&customer)
+	utilities.CheckErr(err)
+	db.CreateCustomer(customer)
+	return http.StatusOK
+}
