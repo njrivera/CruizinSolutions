cdf3d46bd5ce60e9d9f2a0a1684c1045ce9a296f
nato <natanael.j.rivera@gmail.com>
2017-08-07 00:54:29 -0400

    fixed bugs with grids

--- cruizinweb/src/components/CustomerGrid.js
+++ cruizinweb/src/components/CustomerGrid.js
@@ -1,35 +1,69 @@
 import React from 'react';
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
-import {Container, Row, Col, Button, Modal, ModalFooter, ModalBody, ModalHeader} from 'reactstrap';
+import GridModal from './GridModal';
+import {Container, Row, Col, Button} from 'reactstrap';
 import axios from 'axios';
 
 export default class CustomerGrid extends React.Component {
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
         this.state = {
-            customer: [],
-            modal: false
+            customers: [],
+            modal: false,
+            id: null
         };
-        var selected;
-        const scope = this;
+        this.loadCustomers();
+        this.onSelectRec = this.onSelectRec.bind(this);
+        this.toggle = this.toggle.bind(this);
+    }
+
+    onSelectRec(row, isSelected) {
+        if (isSelected) {
+            this.selected = row;
+            this.customer = row;
+        }
+        else
+            this.selected = null;
+    }
+
+    loadCustomers() {
         axios.get('/customers')
         .then(response => {
-            scope.setState({customers: response.data});
+            this.setState({customers: response.data});
         })
         .catch(error => {
             console.log(error);
         });
-
-        this.toggle = this.toggle.bind(this)
     }
 
-    onSelectRec(row, isSelected) {
-        if (isSelected)
-            this.selected = row;
-    }
-
-    toggle() {
+    toggle(act) {
+        this.loadCustomers();
+        if(act === 'deleted') {
+            this.selected = null;
+            this.customer = null;
+        }
+        if(act === 'edit' || act === 'delete') {
+            if (!this.selected)
+                return;
+            this.customer = this.selected;
+        }
+        if(act === 'add')
+            this.customer = {
+                cid: '',
+                name: '',
+                address: '',
+                city: '',
+                state: '',
+                zipcode: '',
+                phone: ''
+            };
         this.setState({modal: !this.state.modal});
+        if(act === 'done') {
+            this.customer = null;
+            return;
+        }
+        this.setState({option: act});
+        this.setState({id: this.customer.cid})
     }
 
     render() {
@@ -63,18 +97,19 @@ export default class CustomerGrid extends React.Component {
                     </Row>
                     <Row>
                         <Col>
-                            <Button color='success' onClick={this.toggle}>Add</Button>
-                            <Button color='info'>Edit</Button>
-                            <Button color='danger'>Delete</Button>
+                            <Button color='success' onClick={() => this.toggle('add')}>Add</Button>
+                            <Button color='info' onClick={() => this.toggle('edit')}>Edit</Button>
+                            <Button color='danger' onClick={() => this.toggle('delete')}>Delete</Button>
                         </Col>
                     </Row>
                 </Container>
-
-                <Modal isOpen={this.state.modal} toggle={this.toggle}>
-                    <ModalHeader>sdf</ModalHeader>
-                    <ModalBody>asdfasdf</ModalBody>
-                    <ModalFooter><Button>sdfsdf</Button></ModalFooter>
-                </Modal>
+                <GridModal 
+                    toggle={this.toggle}
+                    data={this.customer}
+                    id={this.state.id}
+                    action={this.state.option} 
+                    modal={this.state.modal} 
+                    url='/customers'/>
             </div>
         );
     }
